dc6c56f44526a8d6fdcd409933cbff19c6793138
Hans Schroeder <hajschroeder@gmail.com>
2023-04-26 19:56:43 -0700

    include copy-to-clipboard functionality

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,42 @@
-
+import Card from "../UI/Card"
+import * as FaIcons from 'react-icons/fa';
+import * as MdIcons from 'react-icons/md';
+import * as RxIcons from 'react-icons/rx'
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 const Contact = () => {
+
+
+  const [isCopiedState, setIsCopiedState] = useState(false);
+  const email = 'email@todayisfun.com'
+  const copyHandler = (copyEvent) => {
+    copyEvent.preventDefault();
+    return (
+      alert('copied')
+    )
+    
+  }
   return (
     <div>
     <h2>Contact</h2>
-    <a href="https://github.com/hajschroeder/react-music-tutor-beta">Github</a>
+      <Card>
+        <ul>
+          <li>
+            <FaIcons.FaGithub />
+            <a href="https://github.com/hajschroeder/react-music-tutor-beta">Github</a>
+          </li>
+          <li>
+            <div>
+            <MdIcons.MdAlternateEmail />
+            <CopyToClipboard text={email} onCopy={()=> setIsCopiedState(true)}>
+              <a href={email} onClick={copyHandler}>email</a>
+            </CopyToClipboard>
+              {/* <RxIcons.RxCopy /> */}
+            </div>
+
+          </li>
+        </ul>
+      </Card>
     </div>
     )
 }
