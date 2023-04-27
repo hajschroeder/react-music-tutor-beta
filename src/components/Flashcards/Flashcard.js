5f184d4390a8766249b5139a3e3bff73d7204f91
Hans Schroeder <hajschroeder@gmail.com>
2023-04-26 22:10:52 -0700

    style flashcard

--- src/components/Flashcards/Flashcard.js
+++ src/components/Flashcards/Flashcard.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import Card from "../UI/Card";
+import './Flashcard.css'
 
 const Flashcard = ({ flashcard }) => {
   const [cardFlipState, setCardFlipState] = useState(false)
   return (
     <Card>
-      <div onClick={()=> setCardFlipState(!cardFlipState)}>
-        { cardFlipState ? flashcard.answer : flashcard.question}
+      <div className={`flashcard ${cardFlipState ? 'flip' : ' '}`} onClick={()=> setCardFlipState(!cardFlipState)}>
+        <div className="front">
+          {flashcard.question}
+        {/* { cardFlipState ? flashcard.answer : flashcard.question} */}
+        </div>
+        <div className="back">{flashcard.answer}</div>
       </div>
     </Card>
   )  
